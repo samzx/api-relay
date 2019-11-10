b7215f4dbc4b8d5105aba0cb2ce61209ae6c4d83
Sam Xie <sxie@zendesk.com>
2019-11-10 13:36:48 +1100

    mvp

---
+++ cmd/api-relay/api-relay.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// GOOS=linux GOARCH=amd64 go build
+func main() {
+	port := ":2001"
+	fmt.Println("Running on port " + port)
+	http.HandleFunc("/", RelayServer)
+	http.ListenAndServe(port, nil)
+}
+
+func RelayServer(w http.ResponseWriter, r *http.Request) {
+
+	// Enable CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	target := r.URL
+
+	relay, err := fetch(target)
+
+	if err == nil {
+		fmt.Fprintf(w, "%s", relay)
+	} else {
+		fmt.Fprintf(w, "{error: %s}", err)
+	}
+}
+
+func fetch(url *url.URL) (string, error) {
+	// Form proper https URL
+	urlString := "https://" + url.String()[1:]
+
+	// Fetch
+	resp, err := http.Get(urlString)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Return body
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return string(body), err
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/samzx/api-relay
 
 go 1.13
+
+require (
+	github.com/canthefason/go-watcher v0.2.4 // indirect
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	golang.org/x/sys v0.0.0-20191105231009-c1f44814a5cd // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+)
